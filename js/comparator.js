eef1bd074c4f25918e1758ec902d99e84e2235e4
Tolokoban <fabien.petitjean@free.fr>
2017-08-14 22:13:05 +0200

    First release.

---
+++ js/comparator.js
@@ -0,0 +1,163 @@
+/** @module comparator */require( 'comparator', function(require, module, exports) { var _=function(){var D={"en":{},"fr":{}},X=require("$").intl;function _(){return X(D,arguments);}_.all=D;return _}();
+    "use strict";
+
+var $ = require("dom");
+var DB = require("tfw.data-binding");
+var Button = require("wdg.button");
+var Canvas3D = require("canvas-3d");
+
+var WIDTH = 340;
+var HEIGHT = 200;
+
+
+/**
+ * @class Comparator
+ *
+ * @param {boolean} opts.visible - Set the visiblity of the component.
+ *
+ * @example
+ * var Comparator = require("comparator");
+ * var instance = new Comparator({visible: false});
+ */
+var Comparator = function(opts) {
+  var that = this;
+
+  var canvas1 = $.tag( "canvas", "thm-ele2", {
+    width: WIDTH, height: HEIGHT
+  });
+  var canvas2 = $.tag( "canvas", "thm-ele2", {
+    width: WIDTH, height: HEIGHT
+  });
+  var btnCompare = new Button({
+    flat: true,
+    wide: true,
+    icon: "gear",
+    text: "Start a speed test"
+  });
+  var divResult = $.div();
+  var elem = $.elem( this, 'div', 'comparator', [
+    $.div( "flex", [canvas1, canvas2] ),
+    $.div( "flex", [btnCompare, divResult] ),
+  ] );
+
+  DB.prop( this, 'slot' )(function(slot) {
+    if( typeof slot === "string" ) {
+      slot = APP[slot];
+    }
+    if( typeof slot === "function" ) {
+      loadImages( that.images ).then(function( images ) {
+        var ctx1 = canvas1.getContext("2d");
+        try {
+          slot( ctx1, images );
+        } catch( ex ) {
+          console.error( Error( ex ) );
+        }
+        var ctx2 = new Canvas3D( canvas2 );
+        try {
+          slot( ctx2, images );
+        } catch( ex ) {
+          console.error( Error( ex ) );
+        }
+      });
+    }
+  });
+
+  DB.propStringArray( this, 'images' );
+
+  btnCompare.on(function() {
+    var slot = that.slot;
+    if( typeof slot === "string" ) {
+      slot = APP[slot];
+    }
+
+    var c1 = document.createElement("canvas");
+    c1.setAttribute("width", WIDTH);
+    c1.setAttribute("height", HEIGHT);
+    var c2 = document.createElement("canvas");
+    c2.setAttribute("width", WIDTH);
+    c2.setAttribute("height", HEIGHT);
+    var ctx1 = c1.getContext("2d");
+    var ctx2 = c2.getContext("2d");
+    var time1;
+    var time2;
+    var LOOPS = 10000;
+    var loop;
+
+    divResult.textContent = "---";
+    btnCompare.enabled = false;
+
+    loadImages( that.images ).then(function( images ) {
+      loop = LOOPS;
+      time2 = window.performance.now();
+      while( loop --> 0 ) {
+        slot( ctx2, images );
+      }
+      time2 = window.performance.now() - time2;
+
+      loop = LOOPS;
+      time1 = window.performance.now();
+      while( loop --> 0 ) {
+        slot( ctx1, images );
+      }
+      time1 = window.performance.now() - time1;
+
+      var result = Math.floor( 0.5 + 100 * (time2 / time1) );
+      divResult.textContent = result + " %";
+      btnCompare.enabled = true;
+    });
+  });
+
+  opts = DB.extend({
+    images: [],
+    slot: null
+  }, opts, this);
+};
+
+
+module.exports = Comparator;
+
+
+
+function loadImages( images ) {
+  var result = {};
+  return new Promise(function (resolve, reject) {
+    if( !Array.isArray( images ) || images.length === 0 ) {
+      resolve( result );
+      return;
+    }
+
+    var count = images.length;
+    images.forEach(function (src) {
+      var img = new Image();
+      img.onload = function() {
+        result[src] = img;
+        count--;
+        if( count <= 0 ) {
+          resolve( result );
+        }
+      };
+      img.onerror = function() {
+        console.error( "Unable to load image: ", src );
+        count--;
+        if( count <= 0 ) {
+          resolve( result );
+        }
+      };
+      img.src = "css/main/" + src + ".png";
+    });
+  });
+}
+
+
+  
+module.exports._ = _;
+/**
+ * @module comparator
+ * @see module:$
+ * @see module:dom
+ * @see module:tfw.data-binding
+ * @see module:wdg.button
+ * @see module:canvas-3d
+
+ */
+});
---
+++ js/main.js
@@ -0,0 +1,116 @@
+/** @module main */require( 'main', function(require, module, exports) { var _=function(){var D={"en":{"welcome":"Welcome in the world of"},"fr":{"welcome":"Bienvenue dans le monde de"}},X=require("$").intl;function _(){return X(D,arguments);}_.all=D;return _}();
+    "use strict";
+
+require("font.josefin");
+
+var $ = require("dom");
+
+
+module.exports.fillRect = function( ctx ) {
+  // #(fillRect)
+  ctx.fillRect( 10, 10, 200, 100 );
+  // #(fillRect)
+};
+
+module.exports.fillRectWithFillStyle = function( ctx ) {
+  // #(fillRectWithFillStyle)
+  ctx.fillStyle = "#f00";
+  ctx.fillRect( 10, 10, 260, 120 );
+  ctx.fillStyle = "#28af47";
+  ctx.fillRect( 20, 20, 260, 120 );
+  ctx.fillStyle = "hsl(17,70%,50%)";
+  ctx.fillRect( 30, 30, 260, 120 );
+  ctx.fillStyle = "#7733fd";
+  ctx.fillRect( 40, 40, 260, 120 );
+  ctx.fillStyle = "#F39C15";
+  ctx.fillRect( 50, 50, 260, 120 );
+  // #(fillRectWithFillStyle)
+};
+
+module.exports.fillRectWithFillStyleAndAlpha = function( ctx ) {
+  // #(fillRectWithFillStyleAndAlpha)
+  ctx.globalAlpha = 0.3;
+  ctx.fillStyle = "#f00";
+  ctx.fillRect( 10, 10, 260, 120 );
+  ctx.fillStyle = "#0f0";
+  ctx.fillRect( 20, 25, 260, 120 );
+  ctx.fillStyle = "#00f";
+  ctx.fillRect( 30, 40, 260, 120 );
+  ctx.fillStyle = "#73d";
+  ctx.fillRect( 40, 55, 260, 120 );
+  // #(fillRectWithFillStyleAndAlpha)
+};
+
+module.exports.translate = function( ctx ) {
+  // #(translate)
+  ctx.translate( 100, 50 );
+  ctx.fillStyle = "#f00";
+  ctx.fillRect( 0, 0, 200, 100 );
+  ctx.translate( -60, 20 );  
+  ctx.fillStyle = "#00f";
+  ctx.fillRect( 0, 0, 200, 100 );
+  // #(translate)
+};
+
+module.exports.rotate = function( ctx ) {
+  // #(rotate)
+  ctx.rotate( Math.PI / 6 );
+  ctx.fillRect( 50, -25, 200, 100 );
+  // #(rotate)
+};
+
+module.exports.rotateAndTranslate = function( ctx ) {
+  // #(rotateAndTranslate)
+  ctx.translate( ctx.canvas.width * 0.5, ctx.canvas.height * 0.5 );
+  ctx.rotate( -Math.PI / 13 );
+  ctx.translate( -100, -50 );
+  ctx.fillRect( 0, 0, 200, 100 );
+  // #(rotateAndTranslate)
+};
+
+module.exports.saveRestore = function( ctx ) {
+  // #(saveRestore)
+  ctx.fillStyle = "#0f4";
+  for( var a=0 ; a < 25 ; a++ ) {
+    ctx.save();
+    ctx.fillStyle = "hsl(" + 14.4 * a + " 100% 50%)";
+    ctx.translate( ctx.canvas.width * 0.5, ctx.canvas.height * 0.5 );
+    ctx.rotate( a * Math.PI * 0.04 );
+    ctx.translate( -100, -5 );
+    ctx.fillRect( 0, 0, 200, 10 );
+    ctx.restore();
+  }
+  ctx.fillRect( 0, 0, 50, 50 );
+  // #(saveRestore)
+};
+
+module.exports.drawImage = function( ctx, images ) {
+  // #(drawImage)
+  var W = ctx.canvas.width;
+  var H = ctx.canvas.height;
+  var sprite1 = images.sprite1;
+
+  ctx.drawImage( images.sprite1, 0, 0 );
+  ctx.drawImage( images.sprite2, W - 160, 0 );
+  ctx.save();
+  ctx.globalAlpha = 0.7;
+  ctx.translate( 0.5 * W, 0.5 * H );
+  ctx.rotate( Math.PI / 3 );
+  ctx.scale( 0.5, 0.5 );
+  ctx.translate( -0.5 * sprite1.width, -0.5 * sprite1.height );
+  ctx.drawImage( images.sprite1, 0, 0 );
+  ctx.restore();
+  // #(drawImage)
+};
+
+
+  
+module.exports._ = _;
+/**
+ * @module main
+ * @see module:$
+ * @see module:font.josefin
+ * @see module:dom
+
+ */
+});
